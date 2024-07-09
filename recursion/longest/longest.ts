c2f3d2f8f28771fd52a1cbd305dbf13eea037bf3
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-07-09 18:27:25 -0400

    Add code for isPalindrome that passes tests

--- recursion/longest/longest.ts
+++ recursion/longest/longest.ts
@@ -3,9 +3,7 @@ import { LLNodeStr } from "../common/ll";
 /** longest: return length of longest word in a linked list of words. */
 
 function longest(words: LLNodeStr | null): number {
-
   if (words === null) return 0;
-
 
   return Math.max(words.val.length, longest(words.next));
 }
