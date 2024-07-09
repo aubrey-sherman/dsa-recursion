55c8e3831d806b1128c82438b5e266caa6256f90
Zach Bunting <zvrbunting@gmail.com>
2024-07-09 15:38:41 -0700

    Finished revString

--- recursion/rev-string/revString.ts
+++ recursion/rev-string/revString.ts
@@ -1,7 +1,30 @@
 /** revString: return a copy of a string, but in reverse. */
 
 function revString(str: string): string {
-  return "x";
+  if (str.length < 2) return str;
+
+  return revString(str.slice(1)) + str[0];
 }
 
 export { revString };
+
+/*
+base case:
+recursive case:
+
+"abc" -> "cba"
+
+"abc"
+"bc"
+"c"
+
+c + b + a
+
+simple cases:
+length 0
+"" -> ""
+length 1
+"s" -> "s"
+
+
+*/
