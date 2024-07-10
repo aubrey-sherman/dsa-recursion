72126f4b976c8516d90bd5fc6c3f92817596ff11
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-07-09 21:20:00 -0400

    Add code for binarySearchIndex that passes tests

--- recursion/binary-search-index/binarySearchIndex.ts
+++ recursion/binary-search-index/binarySearchIndex.ts
@@ -6,7 +6,26 @@ function binarySearchIndex(
     val: number,
     left = 0,
     right = arr.length): number {
-  return 42;
+
+  if (left === right) return -1;
+
+  const midPoint = Math.floor( (left + right) / 2);
+  if (arr[midPoint] === val) return midPoint;
+  if (arr[midPoint] > val) return binarySearchIndex(arr, val, left, midPoint);
+  return binarySearchIndex(arr, val, midPoint + 1, right);
 }
 
 export { binarySearchIndex };
+
+
+/*
+
+binarySearchIndex([1,2,3,4], 3); // 2
+
+if curr val equals 3 return the index
+if curr val is greater than 3,
+otherwise return binarySearchIndex(arr )
+
+binarySearchIndex([1,2,3,4], 3); // 2
+binarySearchIndex([1,2,3,4], 5); // -1
+*/
